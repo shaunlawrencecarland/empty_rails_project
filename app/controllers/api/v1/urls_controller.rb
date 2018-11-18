dd22372ca191ad0a10721640c5dc344881ff482b
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-18 17:18:48 -0500

    Added endpoint

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -8,10 +8,20 @@ module Api
       private :create_url_params
 
       def create
-        if Url.valid_url?(create_url_params)
-          render json: { url: create_url_params }, status: :ok
+        unless Url.valid_url?(create_url_params)
+          return render json: { error: "input url is not valid" }, status: :unprocessable_entity
+        end
+
+        ActiveRecord::Base.transaction do
+          @url = Url.new(path: params[:url])
+          @url.save!
+          @url.slug = Url.encode(@url.id)
+        end
+
+        if !@url.nil? && @url.save
+          return render json: @url, status: :ok
         else
-          render json: { error: "input url is not valid" }, status: :unprocessable_entity
+          return render json: { error: "url was not shortened" }, status: :unprocessable_entity
         end
       end
     end
