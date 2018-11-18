873ebf939d621563c319723ecdd723c7ea974f09
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-18 12:23:27 -0500

    Add in valid_url? method

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Url < ApplicationRecord
-
+  URL_REGEX = /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/
   # Take in a given URL and determine if it is a valid URL
   # Input [String] url
   # Output [Boolean]
   def self.valid_url?(url)
+    !!(url.match(URL_REGEX))
   end
   # Take in a URL and create a shortened version of the URL
   # Input: [String] url  - The full URL
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -3,9 +3,50 @@ RSpec.describe Url, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
   describe "self.valid_url?" do
-    describe "when the URL is valid" do
+    context "invalid urls" do
+      let(:url) { "foo" }
+
+      it "returns false" do
+        expect(described_class.valid_url?(url)).to eq(false)
+      end
+    end
+    context "valid urls" do
+      let(:url) { "google.com" }
+
       it "returns true" do
-        expect(described_class.valid_url?("http://www.google.com")).to eq(true)
+        expect(described_class.valid_url?(url)).to eq(true)
+      end
+
+      describe "when the url has multiple domain extensions" do
+        let(:url) { "google.co.uk" }
+
+        it "returns true" do
+          expect(described_class.valid_url?(url)).to eq(true)
+        end
+      end
+
+      describe "when the url includes www" do
+        let(:url) { "www.google.com" }
+
+        it "returns true" do
+          expect(described_class.valid_url?(url)).to eq(true)
+        end
+      end
+
+      describe "when the url includes http://" do
+        let(:url) { "http://www.google.com" }
+
+        it "returns true" do
+          expect(described_class.valid_url?(url)).to eq(true)
+        end
+      end
+
+      describe "when the url includes https://" do
+        let(:url) { "https://www.google.com" }
+
+        it "returns true" do
+          expect(described_class.valid_url?(url)).to eq(true)
+        end
       end
     end
   end
