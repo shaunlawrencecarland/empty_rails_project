69b71d08a36e5c7c59e6bb484e95d88571cc6260
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-18 17:08:39 -0500

    failing spec

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -4,11 +4,22 @@ RSpec.describe Api::V1::UrlsController, type: :controller do
     describe "when the URL is in valid format" do
       before(:each) do
         allow(Url).to receive(:valid_url?).and_return(true)
-        post :create, params: { url: "good url" }
+        allow(Url).to receive(:encode).and_return("encoded_slug")
       end
 
       it "returns a 200 response" do
+        post :create, params: { url: "good url" }
         expect(response.status).to eq(200)
+      end
+
+      it "creates a new URL object with correct path and slug" do
+        expect { post :create, params: { url: "good url" } }
+          .to change{Url.count}.from(0).to(1)
+
+        url = Url.first
+
+        expect(url.slug).to eq("encoded_slug")
+        expect(url.path).to eq("good url")
       end
     end
 
