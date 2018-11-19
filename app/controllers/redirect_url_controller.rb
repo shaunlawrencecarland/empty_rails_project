d8200da654a7251553dc9f5c26b1cc707487a460
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-19 13:23:40 -0500

    wip

--- app/controllers/redirect_url_controller.rb
+++ app/controllers/redirect_url_controller.rb
@@ -1,5 +1,18 @@
 class RedirectUrlController < ApplicationController
   def index
+    @slug = params[:slug]
+    # test to see if the slug is valid
+      # if not, return :unprocessable_entity status
+    # test to see if the slug exists in the DB
+      # if not, return :unprocessable_entity status
+    unless Url.valid_slug?(params[:slug])
+      render json: {error: "Slug not valid.  Slug must contain only alphanumeric characters"}, status: :unprocessable_entity
+    end
+
+    url_id = UrlHelper.decode(@slug)
+    @url = Url.find(url_id)
+
+    
     render json: {slug: params[:slug]}, status: :ok
   end
 end
---
+++ spec/controllers/redirect_url_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+RSpec.describe UrlsController, type: :controller do
+  describe ""
+end
