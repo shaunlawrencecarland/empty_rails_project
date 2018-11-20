1b68f89cc1d176de49fa53456f68b43601c1015a
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-19 23:50:41 -0500

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  
-  namespace :api do
-    namespace :v1 do
-      resources :urls, only: [:index, :create]
-    end
-  end
+  resources :urls, only: [:index, :create]
 
   get "*slug", to: "redirect_url#index"
 end
