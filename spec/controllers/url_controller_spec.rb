99c7709ef03e1dd3b0085c25462144b76a96cd5e
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-20 15:37:08 -0500

    specs passing

--- spec/controllers/url_controller_spec.rb
+++ spec/controllers/url_controller_spec.rb
@@ -1,37 +1,39 @@
 require 'rails_helper'
 RSpec.describe Api::V1::UrlsController, type: :controller do
+
   describe "#create" do
+    let(:subject) { post :create, params: params }
     describe "when the URL is in valid format" do
+      let(:params) { { url: { path: "google.com" } } }
+
       it "returns a 200 response" do
-        post :create, params: { url: "google.com" }
+        subject
         expect(response.status).to eq(200)
       end
 
       it "creates a new URL object with correct path and slug" do
-        expect { post :create, params: { url: "http://www.google.com" } }
+        expect { subject }
           .to change{Url.count}.from(0).to(1)
 
         url = Url.first
 
         expect(url.slug).to eq("b")
-        expect(url.path).to eq("http://www.google.com")
+        expect(url.path).to eq("http://google.com")
       end
-    end
 
-    describe "when there is an error with saving the url" do
-      before(:each) { allow_any_instance_of(Url).to receive(:save).and_return(false) }
+      describe "and there is an error with saving the url" do
+        before(:each) { allow_any_instance_of(Url).to receive(:save).and_return(false) }
 
-      it "logs an error message" do
-        expect(Rails.logger).to receive(:error)
-        post :create, params: { url: "http://google.com" }
+        it "logs an error message" do
+          expect(Rails.logger).to receive(:error)
+          subject
+        end
       end
     end
 
     describe "when the URL is not in a valid format" do
-      before(:each) do
-        allow(Url).to receive(:valid_url?).and_return(false)
-        post :create, params: { url: "bad url" }
-      end
+      let(:params) { { url: { path: "bad url" } } }
+      before(:each) { subject }
 
       it "returns a 400 status code" do
         expect(response.status).to eq(422)
