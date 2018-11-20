ede8b1f340f53e85976dbf31a67b515472483f51
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-20 16:08:45 -0500

    Clean up comments

--- db/migrate/20181120210316_add_index_to_urls_path.rb
+++ db/migrate/20181120210316_add_index_to_urls_path.rb
@@ -1,8 +1,6 @@
 class AddIndexToUrlsPath < ActiveRecord::Migration[5.2]
   def change
-    # add_column :moderators, :username, :string unless column_exists? :moderators, :username
     add_column :urls, :path, :string unless column_exists? :urls, :path
     add_index :urls, :path, unique: true
-    # t.references :urls, :path, unique: true
   end
 end
