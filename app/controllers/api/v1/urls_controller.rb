c50234da141a07c49fde33bce77df5dd19f2301f
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-20 14:32:21 -0500

    Add back versioning, add urls controller index page, remove old urls controller

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -7,27 +7,23 @@ module Api
       end
       private :create_url_params
 
+      def index
+        render json: { foo: "bar" }
+      end
+
       def create
-        # binding.pry
-
-        # return render json: { params: params, create_url_params: create_url_params }
-
         unless Url.valid_url?(create_url_params)
           return render json: { error: "input url is not valid" }, status: :unprocessable_entity
         end
 
         ActiveRecord::Base.transaction do
-          @url = Url.new(path: create_url_params)
+          @url = Url.new(path: params[:url])
           @url.save!
           @url.slug = UrlHelper.encode(@url.id)
         end
 
         if !@url.nil? && @url.save
-          flash[:success] = "Short URL Created!  The slug is #{@url.slug}"
-          redirect_to :root
-          # return redirect_to "home#index"
-
-          # return render json: @url, status: :ok
+          return render json: @url, status: :ok
         else
           log_msg = "Error creating URL. URL parameter: #{create_url_params} URL Object: #{@url.to_json}"
           Rails.logger.error log_msg
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root "home#index"
-  resources :urls, only: [:index, :create]
+  # root "home#index"
+  root "api/v1/urls#index"
 
+  namespace :api do
+    namespace :v1 do
+      resources :urls, only: [:index, :create]
+    end
+  end
   get "*slug", to: "redirect_url#index"
 end
