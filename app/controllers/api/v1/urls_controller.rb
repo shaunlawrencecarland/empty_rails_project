7918b058c16ea97957ebae3d3d98d113baf5e9f1
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-20 15:31:43 -0500

    rename create_url_params to url_params

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -2,10 +2,10 @@ module Api
   module V1
     class UrlsController < ApplicationController
       # TODO: Can I make this better??
-      def create_url_params
+      def url_params
         params.require(:url).permit(:path)
       end
-      private :create_url_params
+      private :url_params
 
       def index
         @urls = Url.order(hit_count: :desc).limit(100)
@@ -13,12 +13,12 @@ module Api
       end
 
       def create
-        unless Url.valid_url?(create_url_params["path"])
+        unless Url.valid_url?(url_params["path"])
           return render json: { error: "input url is not valid" }, status: :unprocessable_entity
         end
 
         ActiveRecord::Base.transaction do
-          @url = Url.new(path: create_url_params["path"])
+          @url = Url.new(path: url_params["path"])
           @url.save!
           @url.slug = UrlHelper.encode(@url.id)
         end
@@ -26,7 +26,7 @@ module Api
         if !@url.nil? && @url.save
           return render json: @url, status: :ok
         else
-          log_msg = "Error creating URL. URL parameter: #{create_url_params} URL Object: #{@url.to_json}"
+          log_msg = "Error creating URL. URL parameter: #{url_params} URL Object: #{@url.to_json}"
           Rails.logger.error log_msg
           return render json: { error: "url was not shortened" }, status: :unprocessable_entity
         end
