b373d75346bcb249c5c26261b094fbf1826c515f
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-21 15:42:46 -0500

    wip

--- app/helpers/url_constructor.rb
+++ app/helpers/url_constructor.rb
@@ -5,22 +5,23 @@ class UrlConstructor
     ActiveRecord::Base.transaction do
       begin
         if @url.valid?
-          break if path_already_exists?
-          break unless save_url!
-          @slug = UrlHelper.encode(@url.id)
-          @url.slug = UrlHelper.encode(@url.id)
-          break unless save_url!
+          if !path_already_exists?
+            break unless save_url!
+            @slug = UrlHelper.encode(@url.id)
+            @url.slug = UrlHelper.encode(@url.id)
+            break unless save_url!
+          end
         end
       rescue ActiveRecord::RecordNotUnique => e
-        existing_url = Url.where(path: @url.path)
-
-        # first_ex = existing_url.first
-        # puts "~~~: #{existing_url}"
-        # first =
-        # existing_slug = existing_url.slug
-        msg = "error to s: #{e.to_s} error inspect: #{e.inspection} error"
-        # msg = "URL #{path} already exists.  Its slug is #{existing_url.slug}"
-        @url.errors.add(:path, msg)
+        # existing_url = Url.where(path: @url.path)
+        #
+        # # first_ex = existing_url.first
+        # # puts "~~~: #{existing_url}"
+        # # first =
+        # # existing_slug = existing_url.slug
+        # msg = "error to s: #{e.to_s} error inspect: #{e.inspection} error"
+        # # msg = "URL #{path} already exists.  Its slug is #{existing_url.slug}"
+        # @url.errors.add(:path, msg)
       end
     end
     @url
