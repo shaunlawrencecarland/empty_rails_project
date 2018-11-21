77b59c2a7266b6b3905278b6c25cdee426ce21ad
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-11-21 15:59:36 -0500

    fix method name

--- app/helpers/url_constructor.rb
+++ app/helpers/url_constructor.rb
@@ -19,7 +19,7 @@ class UrlConstructor
         # # puts "~~~: #{existing_url}"
         # # first =
         # # existing_slug = existing_url.slug
-        msg = "error to s: #{e.to_s} error inspect: #{e.inspection} error"
+        msg = "error to s: #{e.to_s} error inspect: #{e.inspect} error cause: #{e.cause} "
         # msg = "URL #{path} already exists.  Its slug is #{existing_url.slug}"
         @url.errors.add(:path, msg)
       end
