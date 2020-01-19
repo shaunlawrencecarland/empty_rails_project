dd4b4e20e411e52d1e0fabde740db417a6e728da
Shaun <shauncarland@gmail.com>
2020-01-18 20:49:10 -0500

    remove old files

--- app/models/url.rb
+++
@@ -1,4 +0,0 @@
-class Url < ApplicationRecord
-  validates_format_of :path, with: UrlHelper.url_regex,
-    :message => "URL is in an invalid format"
-end
--- spec/factories/url.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :url do
-
-  end
-end
