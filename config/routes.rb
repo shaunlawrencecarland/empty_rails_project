76171465478727c29c09bb1dc948881506018bf3
Shaun <shauncarland@gmail.com>
2020-01-18 20:51:56 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
   resources :foos
-  # root "home#index"
-  root "api/v1/urls#index"
-
-  namespace :api do
-    namespace :v1 do
-      resources :urls, only: [:index, :create]
-    end
-  end
-  get "*slug", to: "redirect_url#index"
 end
